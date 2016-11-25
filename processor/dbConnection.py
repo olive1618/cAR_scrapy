c811bf7cec69f6ff8e6b2c0611eb30e9d61a88ab
Ryan Carlton <rlc120304@gmail.com>
2016-11-25 12:00:38 -0500

    First Commit with database services.

---
+++ processor/dbConnection.py
@@ -0,0 +1,30 @@
+from pymongo import MongoClient
+
+class DBConnection:
+    dbClient = MongoClient('mongodb://localhost:27017/')
+    database = dbClient['cars-database']
+    collection = database['cars-collection']
+    list_of_items_added = []
+
+    #def __init__(self, name):
+    #    self.name = name
+
+    def create_doc(self, doc):
+        newest_id = self.collection.insert_one(doc)
+        self.list_of_items_added.append(newest_id)
+
+    def remove_doc(self, doc):
+        self.collection.delete_many({"_id":doc['_id']})
+        self.list_of_items_added.remove(doc['_id'])
+
+    def get_all_docs(self):
+        list_of_docs = []
+        for docs in self.collection.find():
+            list_of_docs.append(docs)
+        return list_of_docs
+
+    def update_one_doc(self, doc):
+        self.collection.update_one({'_id': doc['_id']}, doc, upsert=False)
+
+
+
---
+++ processor/mongostartup.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/Applications/mongodb/bin/mongod --dbpath /Applications/mongodb/data/db
